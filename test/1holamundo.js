537225266a30a7b2180ada323dd296ecd4bdf186
ArmandoMtrz <am19133@ues.edu.sv>
2024-10-14 18:58:28 -0600

    pruebas unitarias

---
+++ test/1holamundo.js
@@ -0,0 +1,8 @@
+//José Armando Alarcón Martínez
+const assert = require('chai').assert;
+
+describe('01 prueba hola mundo', () => {
+    it('estoy haciendo la primera prueba', () => {
+        assert.equal(true, true);
+    });
+});
